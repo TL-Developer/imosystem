aa0ab46de2016e7a31e587b605752831ad649334
Tiago Lima <tiago.jlima@yahoo.com.br>
2015-10-09 11:12:15 -0300

    ajustando layout chato online

--- public/js/controllers/admin/UsuarioController.js
+++ public/js/controllers/admin/UsuarioController.js
@@ -99,24 +99,43 @@ angular.module('imobiliaria').controller('UsuarioController', function($scope, $
 	};
 
 
-	var cores = ['alert alert-info', 'alert alert-success', 'alert alert-warning', 'alert alert-danger'],
+
+	// var cores = ['alert alert-info', 'alert alert-success', 'alert alert-warning', 'alert alert-danger'],
+	var cores = ['#ccc', '#ff3300']
 		coresIncrement = 0;
 		socket = io.connect(),
 		$messageBox = $('#message'),
 		$chat = $('#chat');
 
+	
+	var sendMensagem = function(valor, posicao){
+
+		return '<div class="row"><div class="col-md-11">'+
+					'<div class="direct-chat-msg '+posicao+'">'+
+				    '  <div class="direct-chat-info clearfix">'+
+				    '    <span class="direct-chat-name pull-left">'+$scope.usuario.firstName+'</span>'+
+				    '    <span class="direct-chat-timestamp pull-right">'+new Date()+'</span>'+
+				    '  </div>'+
+				    '  <img class="direct-chat-img" alt="message user image" src="img/usuarios/'+$scope.usuario.imagem+'">'+
+				    '  <div class="direct-chat-text">'+valor+'</div>'+
+				    '</div>'+
+				'</div></div>';
+
+	};
+
 	$scope.sendMessage = function(){
-		coresIncrement++;
-		if(coresIncrement == 3){
-			coresIncrement = 0;
-		}
-		socket.emit('send message', '<div class="row"><div class="col-md-12"><img style="width: 35px; height: 35px; float: left; margin: 1% 2% 0 0;" class="img-responsive img-circle" src="img/usuarios/'+$scope.usuario.imagem+'" alt="'+$scope.usuario.firstName+'" title="'+$scope.usuario.firstName+' '+new Date()+'"> <div class="'+cores[coresIncrement]+'" style="float: left;"><h5 class="text-info nopadding">'+$messageBox.val()+'</h5></div></div>  </div');
+
+		socket.emit('send message', sendMensagem($messageBox.val(), 'righ'));
+
+		// socket.emit('send message', '<div class="row"><div class="col-md-12"><img style="width: 35px; height: 35px; float: left; margin: 1% 2% 0 0;" class="img-responsive img-circle" src="img/usuarios/'+$scope.usuario.imagem+'" alt="'+$scope.usuario.firstName+'" title="'+$scope.usuario.firstName+' '+new Date()+'"> <div class="'+cores[coresIncrement]+'" style="float: left;"><h5 class="text-info nopadding">'+$messageBox.val()+'</h5></div></div>  </div');
 		$messageBox.val('');
 	};
 
-
 	socket.on('new message', function(data){
+		
 		$chat.append(data);
+		
 	});
-
 });
+
+// <div class="row"><div class="col-md-12"><img style="width: 35px; height: 35px; float: left; margin: 1% 2% 0 0;" class="img-responsive img-circle" src="img/usuarios/'+$scope.usuario.imagem+'" alt="'+$scope.usuario.firstName+'" title="'+$scope.usuario.firstName+' '+new Date()+'"> <div class="'+cores[coresIncrement]+'" style="float: left;"><h5 class="text-info nopadding"></h5></div></div>  </div>
